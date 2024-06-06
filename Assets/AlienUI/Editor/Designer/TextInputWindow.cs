f0db95be7b8169fb2d901ab2809156ac9c724f86
ALIENJACK\alien <wangshijie>
2024-06-06 15:54:52 +0800

    优化

--- Assets/AlienUI/Editor/Designer/TextInputWindow.cs
+++ Assets/AlienUI/Editor/Designer/TextInputWindow.cs
@@ -15,32 +15,67 @@ public class TextInputWindow : EditorWindow
         wnd.minSize = new Vector2(500, 45);
         wnd.maxSize = wnd.minSize;
 
+        popPos = GUIUtility.GUIToScreenPoint(popPos);
+
+        wnd.firstShow = true;
+        wnd.Show();
 
         var pos = wnd.position;
         pos.position = popPos;
         wnd.position = pos;
 
-        wnd.ShowModalUtility();
-
+        wnd.ShowModal();
     }
 
+    bool firstShow;
     void OnGUI()
     {
+        if (Event.current.type == EventType.KeyDown)
+        {
+            if (Event.current.keyCode == KeyCode.Escape)
+            {
+                Close();
+                Event.current.Use();
+                return;
+            }
+            else if (Event.current.keyCode == KeyCode.KeypadEnter || Event.current.keyCode == KeyCode.Return)
+            {
+                HandleInput();
+                Event.current.Use();
+                return;
+            }
+        }
+
+        if (firstShow)
+            GUI.SetNextControlName("InputFieldNeedFocus");
+
         userInput = EditorGUILayout.TextField(userInput);
+
+        if (firstShow)
+            EditorGUI.FocusTextInControl("InputFieldNeedFocus");
         EditorGUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         if (GUILayout.Button("OK", GUILayout.Width(50)))
         {
-            try
-            {
-                userInputCallback?.Invoke(userInput);
-            }
-            finally
-            {
-                Close();
-            }
+            HandleInput();
         }
         EditorGUILayout.EndHorizontal();
+
+        
+
+        firstShow = false;
+    }
+
+    private void HandleInput()
+    {
+        try
+        {
+            userInputCallback?.Invoke(userInput);
+        }
+        finally
+        {
+            Close();
+        }
     }
 
     private void OnDestroy()
