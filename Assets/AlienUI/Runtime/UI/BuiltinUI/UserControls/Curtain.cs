f87871342ea1beef22b3d38815c49ab6763fffbf
ALIENJACK <alienjack@foxmail.com>
2024-06-12 23:45:48 +0800

    完成帷幕类型控件逻辑。LoginScreen的仿Windows功能制作中

--- Assets/AlienUI/Runtime/UI/BuiltinUI/UserControls/Curtain.cs
+++ Assets/AlienUI/Runtime/UI/BuiltinUI/UserControls/Curtain.cs
@@ -1,3 +1,7 @@
+using AlienUI.Core.Commnands;
+using AlienUI.Events;
+using AlienUI.Models;
+using System;
 using UnityEngine;
 
 namespace AlienUI.UIElements
@@ -6,16 +10,79 @@ namespace AlienUI.UIElements
     {
         public override ControlTemplate DefaultTemplate => new ControlTemplate("Builtin.Curtain");
 
+        public float OpenThreshold
+        {
+            get { return (float)GetValue(OpenThresholdProperty); }
+            set { SetValue(OpenThresholdProperty, value); }
+        }
+
+        public static readonly DependencyProperty OpenThresholdProperty =
+            DependencyProperty.Register("OpenThreshold", typeof(float), typeof(float), new PropertyMetadata(0.3f));
+
+        public float CurtainProgress
+        {
+            get { return (float)GetValue(CurtainProgressProperty); }
+            set { SetValue(CurtainProgressProperty, value); }
+        }
+        public static readonly DependencyProperty CurtainProgressProperty =
+            DependencyProperty.Register("CurtainProgress", typeof(float), typeof(float), new PropertyMetadata(0f), OnCurtainProgressChanged);
+
+
+
+        public CommandBase Command
+        {
+            get { return (CommandBase)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(CommandBase), typeof(Curtain), new PropertyMetadata(null));
+
+
+        private static void OnCurtainProgressChanged(DependencyObject sender, object oldValue, object newValue)
+        {
+            var self = sender as Curtain;
+            var tempRoot = self.m_templateInstance.TemplateRoot.Get(self.m_templateInstance.Document) as UIElement;
+            var currentOffset = tempRoot.ActualHeight * self.CurtainProgress;
+            tempRoot.Offset = new Vector2(0, currentOffset);
+
+            tempRoot.Alpha = 1f - (self.CurtainProgress / self.OpenThreshold);
+        }
+
         protected override void OnInitialized()
         {
             OnDrag += Curtain_OnDrag;
+            OnEndDrag += Curtain_OnEndDrag;
+        }
+
+        private void Curtain_OnEndDrag(object sender, Events.OnEndDragEvent e)
+        {
+            if (CurtainProgress < OpenThreshold)
+            {
+                CurtainProgress = 0f;
+            }
+            else
+            {
+                CurtainProgress = 1f;
+                Command?.Execute();
+                RaiseCustomEvent(new OnCurtainOpened());
+            }
         }
 
         private void Curtain_OnDrag(object sender, Events.OnDragEvent e)
         {
-            var tempRoot = m_templateInstance.TemplateRoot.Get(m_templateInstance.Document) as VisualElement;
-            var delta = new Vector2(0, e.EvtData.delta.y / Canvas.scaleFactor);
-            tempRoot.Offset += delta;
+            var tempRoot = m_templateInstance.TemplateRoot.Get(m_templateInstance.Document) as UIElement;
+
+            var deltaValue = e.EvtData.delta.y / Canvas.scaleFactor;
+            var endValue = tempRoot.ActualHeight;
+
+            var deltaValuePer = deltaValue / endValue;
+            CurtainProgress = Mathf.Clamp01(CurtainProgress + deltaValuePer);
+        }
+
+        public class OnCurtainOpened : AlienUI.Events.Event
+        {
+            public override string EventName => nameof(OnCurtainOpened);
         }
     }
 }
--- Assets/DEMO/Scripts/WindowsEmulator.cs
+++ Assets/DEMO/Scripts/WindowsEmulator.cs
@@ -1,6 +1,7 @@
 using AlienUI;
 using AlienUI.UIElements;
 using AlienUI.UIManager;
+using System.Collections;
 using UnityEngine;
 
 namespace DEMO
@@ -9,10 +10,15 @@ namespace DEMO
     {
         public UIManager UIManager;
 
-        private void Start()
+        private IEnumerator Start()
         {
-            var uiAsset = Settings.Get().GetWindowAsset("LoginScreen");
-            UIManager.OpenWiondow<Window>(uiAsset);            
+            var setting = Settings.Get();
+
+            yield return null;
+
+            var uiAsset = setting.GetWindowAsset("LoginScreen");
+            yield return null;
+            UIManager.OpenWiondow<Window>(uiAsset);
         }
     }
 }
